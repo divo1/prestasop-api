6771a168db73c9327a33cf101dd6cf4e06cffdc8
div <divo@divo.net.pl>
2016-09-19 22:41:20 +0000

    Xml

--- PSWebApi.go
+++ PSWebApi.go
@@ -3,13 +3,11 @@ package psapi
 import (
 	"gopkg.in/resty.v0"
 	"bytes"
+	"fmt"
 	"net/url"
-	"encoding/json"
-	"strings"
+	"encoding/xml"
+	"strconv"
 )
-
-var Limit = 50
-var Unlimited = 999
 
 type prestaService struct {
 	url string
@@ -29,45 +27,83 @@ func NewService(apiUrl string, apiKey string) *prestaService {
 	buffer.WriteString(apiKey)
 	buffer.WriteString("@")
 	buffer.WriteString(url.Host)
-	buffer.WriteString("/")
 	buffer.WriteString(url.Path)
 	buffer.WriteString("/")
 	
 	psService := &prestaService{buffer.String(), apiKey, resty.New()}
 	psService.client.SetHeader("Accept", "application/xml")
+	psService.client.SetHeader("Content-Type", "text/xml")
 	
 	return psService
 }
 
-func (service prestaService) getResource(resource string, limit int) []byte {
+func (service prestaService) getUrl(resource string) bytes.Buffer {
 	var buffer bytes.Buffer
 	buffer.WriteString(service.url)
 	buffer.WriteString(resource)
-	buffer.WriteString("?display=full")
-	buffer.WriteString("&output_format=JSON")
-	buffer.WriteString("&limit=")
-	buffer.WriteString(string(limit))
+	
+	return buffer
+}
 
-	resp, _ := resty.R().Get(buffer.String())
+func (service prestaService) GetResource(resource string, limit int) []byte {
+	Url := service.getUrl(resource)
+	Url.WriteString("?display=full")
+	//Url.WriteString("&output_format=JSON")
+	Url.WriteString("&limit=")
+	Url.WriteString(strconv.Itoa(limit))
+	
+	resp, _ := resty.R().Get(Url.String())
 
 	return resp.Body()
 }
 
-func (service prestaService) GetProducts() []Product {
+func (service prestaService) addResource(resource string, Post []byte) []byte {
+	Url := service.getUrl(resource)
+	resp, _ := resty.R().
+	SetBody(Post).
+	Post(Url.String())
+
+	return resp.Body()
+}
+
+func (service prestaService) GetProducts(Limit int) []Product {
 	resource := "products"
-	obj := make([]Product)
-	json.Unmarshal(service.getResource(resource, Limit), &obj)
-	products := sliceToMapById(obj[strings.ToLower(resource)])
+	var obj struct {
+		Products []Product `xml:"products>product"`
+	}
+	err := xml.Unmarshal(service.GetResource(resource, Limit), &obj)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return obj.Products
+}
+
+func (service prestaService) GetCategories(Limit int) []Category {
+	resource := "categories"
+	var obj struct {
+		Categories []Category `xml:"categories>category"`
+	}
+	err := xml.Unmarshal(service.GetResource(resource, Limit), &obj)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	return obj.Categories
+}
+/*
+func (service prestaService) AddProducts(Product Product) int {
+	resource := "products"
+	
+	
+	
+	var obj map[string][]Product
+	err := xml.Unmarshal(service.getResource(resource, Limit), &obj)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//products := sliceToMapById(obj])
+	products := obj[strings.ToLower(resource)]
 
 	return products
-}
-
-func (service prestaService) GetCategories() map[string]Category {
-	resource := "categories"
-	obj := make([]Category)
-	json.Unmarshal(service.getResource(resource, Unlimited), &obj)
-	
-	for k, element := range obj
-	
-	return obj[strings.ToLower(resource)]
-}
+}*/s
--- models.go
+++ models.go
@@ -1,35 +1,107 @@
 package psapi
 
-import (
-	"time"
-)
-
 type Category struct {
-	Id              int       `json:"id"`
-	IdParent        int       `json:"id_parent"`
-	Active          int       `json:"active"`
-	IdShopDefault   int       `json:"id_shop_default"`
-	IsRootCategory  int       `json:"is_root_category"`
-	Position        int       `json:"position"`
-	Name            string    `json:"name"`
-	LinkRewrite     string    `json:"link_rewrite"`
-	Description     string    `json:"description"`
-	MetaTitle       string    `json:"meta_title"`
-	MetaDescription string    `json:"meta_description"`
-	MetaKeywords    string    `json:"meta_keywords"`
-	DateAdd         time.Date `json:"date_add"`
-	DateUpd         time.Date `json:"date_upd"`
-}
-
-type Associations struct {
-	categoriesId []int
+	ID int `xml:"id"`
+	IDParent string `xml:"id_parent"`
+	LevelDepth string `xml:"level_depth"`
+	Active string `xml:"active"`
+	IDShopDefault string `xml:"id_shop_default"`
+	IsRootCategory string `xml:"is_root_category"`
+	Position string `xml:"position"`
+	DateAdd string `xml:"date_add"`
+	DateUpd string `xml:"date_upd"`
+	Name string `xml:"name"`
+	LinkRewrite string `xml:"link_rewrite"`
+	Description string `xml:"description"`
+	MetaTitle string `xml:"meta_title"`
+	MetaDescription string `xml:"meta_description"`
+	MetaKeywords string `xml:"meta_keywords"`
+	Associations struct {
+		Categories []struct {
+			ID string `xml:"id"`
+		} `xml:"categories"`
+	} `xml:"associations"`
 }
 
 type Product struct {
-	Id int `json:"id"`
-	Quantity int `json:"quantity"`
-	Type string `json:"type"`
-	//Description string `json:"description"`
-	Associations Associations  `json:"associations"`
+	ID string  `xml:"id,attr"`
+	IDManufacturer string `xml:"id_manufacturer"`
+	IDSupplier string `xml:"id_supplier"`
+	IDCategoryDefault string `xml:"id_category_default"`
+	New interface{} `xml:"new"`
+	CacheDefaultAttribute string `xml:"cache_default_attribute"`
+	IDDefaultImage string `xml:"id_default_image"`
+	IDDefaultCombination string `xml:"id_default_combination"`
+	IDTaxRulesGroup string `xml:"id_tax_rules_group"`
+	PositionInCategory string `xml:"position_in_category"`
+	ManufacturerName string `xml:"manufacturer_name"`
+	Quantity string `xml:"quantity"`
+	Type string `xml:"type"`
+	IDShopDefault string `xml:"id_shop_default"`
+	Reference string `xml:"reference"`
+	Sku string `xml:"sku"`
+	SupplierReference string `xml:"supplier_reference"`
+	Location string `xml:"location"`
+	Width string `xml:"width"`
+	Height string `xml:"height"`
+	Depth string `xml:"depth"`
+	Weight string `xml:"weight"`
+	QuantityDiscount string `xml:"quantity_discount"`
+	Ean13 string `xml:"ean13"`
+	Upc string `xml:"upc"`
+	CacheIsPack string `xml:"cache_is_pack"`
+	CacheHasAttachments string `xml:"cache_has_attachments"`
+	IsVirtual string `xml:"is_virtual"`
+	OnSale string `xml:"on_sale"`
+	OnlineOnly string `xml:"online_only"`
+	Ecotax string `xml:"ecotax"`
+	MinimalQuantity string `xml:"minimal_quantity"`
+	Price string `xml:"price"`
+	WholesalePrice string `xml:"wholesale_price"`
+	Unity string `xml:"unity"`
+	UnitPriceRatio string `xml:"unit_price_ratio"`
+	AdditionalShippingCost string `xml:"additional_shipping_cost"`
+	Customizable string `xml:"customizable"`
+	TextFields string `xml:"text_fields"`
+	UploadableFiles string `xml:"uploadable_files"`
+	Active string `xml:"active"`
+	RedirectType string `xml:"redirect_type"`
+	IDProductRedirected string `xml:"id_product_redirected"`
+	AvailableForOrder string `xml:"available_for_order"`
+	AvailableDate string `xml:"available_date"`
+	Condition string `xml:"condition"`
+	ShowPrice string `xml:"show_price"`
+	Indexed string `xml:"indexed"`
+	Visibility string `xml:"visibility"`
+	AdvancedStockManagement string `xml:"advanced_stock_management"`
+	DateAdd string `xml:"date_add"`
+	DateUpd string `xml:"date_upd"`
+	PackStockType string `xml:"pack_stock_type"`
+	MetaDescription string `xml:"meta_description"`
+	MetaKeywords string `xml:"meta_keywords"`
+	MetaTitle string `xml:"meta_title"`
+	LinkRewrite string `xml:"link_rewrite"`
+	Name string `xml:"name"`
+	Description string `xml:"description"`
+	DescriptionShort string `xml:"description_short"`
+	AvailableNow string `xml:"available_now"`
+	AvailableLater string `xml:"available_later"`
+	Associations struct {
+		Categories []struct {
+			ID string `xml:"id"`
+		} `xml:"categories"`
+		Images []struct {
+			ID string `xml:"id"`
+		} `xml:"images"`
+		Combinations []struct {
+			ID string `xml:"id"`
+		} `xml:"combinations"`
+		ProductOptionValues []struct {
+			ID string `xml:"id"`
+		} `xml:"product_option_values"`
+		StockAvailables []struct {
+			ID string `xml:"id"`
+			IDProductAttribute string `xml:"id_product_attribute"`
+		} `xml:"stock_availables"`
+	} `xml:"associations"`
 }
-
